1511331ff979e1ccb7dfd078609b22041e88e3fe
Mincho Velev <mincho.velev@sap.com>
2024-12-02 14:54:26 +0200

    2024 Day2

---
+++ elixir/y2024/day2.ex
@@ -0,0 +1,61 @@
+defmodule Day2 do
+  def read() do
+    f = Path.absname("inputs/day2.txt")
+
+    File.stream!(f)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(fn s -> String.split(s) end)
+    |> Enum.map(fn l -> Enum.map(l, &String.to_integer/1) end)
+  end
+
+  def day2_1() do
+    read()
+    |> IO.inspect()
+    |> Enum.filter(&is_safe/1)
+    |> Enum.count()
+    |> IO.puts()
+  end
+
+  def is_safe(list) do
+    first = Enum.at(list, 0)
+    second = Enum.at(list, 1)
+
+    order = first > second
+
+    has_unsafe =
+      Enum.chunk_every(list, 2, 1, :discard)
+      |> Enum.map(fn [left, right | _] -> {abs(left - right), order == left > right} end)
+      |> Enum.any?(fn {n, o} -> n > 3 or n < 1 or !o end)
+
+    !has_unsafe
+  end
+
+  def day2_2() do
+    read()
+    |> IO.inspect()
+    |> Enum.filter(&has_any_safe/1)
+    |> Enum.count()
+    |> IO.puts()
+  end
+
+  def has_any_safe(list) do
+    lastIndex = Enum.count(list) - 1
+
+    Enum.map(-1..lastIndex, fn index -> sublist(list, index) end)
+    |> Enum.any?(&is_safe/1)
+  end
+
+  def sublist(list, index) when index >= 0 do
+    List.delete_at(list, index)
+  end
+
+  def sublist(list, index) when index < 0 do
+    list
+  end
+end
+
+IO.puts(:part1)
+Day2.day2_1()
+
+IO.puts("\npart2")
+Day2.day2_2()
