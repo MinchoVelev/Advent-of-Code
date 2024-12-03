423b70d19030ab8e5f6e97954b25c43772d9d6cf
Mincho Velev <mincho.velev@sap.com>
2024-12-03 11:00:55 +0200

    2024 Day3

--- elixir/y2024/day3.ex
+++ elixir/y2024/day3.ex
@@ -1,19 +1,31 @@
 defmodule Day3 do
   def read() do
-    f = Path.absname("inputs/day3.test")
+    f = Path.absname("inputs/day3.txt")
 
-    File.stream!(f)
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(fn s -> String.split(s) end)
-    |> Enum.map(fn l -> Enum.map(l, &String.to_integer/1) end)
+    File.read!(f) |> String.replace("\n", "")
   end
 
   def day3_1() do
-    read()
+    calc_sum(read())
     |> IO.inspect()
   end
 
+  def calc_sum(s) do
+    Regex.scan(~r{mul\((\d+),(\d+)\)}, s)
+    |> Enum.map(fn [_, left, right | _] -> String.to_integer(left) * String.to_integer(right) end)
+    |> Enum.sum()
+  end
+
   def day3_2() do
+    input = "do()" <> read() <> "don't()"
+
+    s =
+      Regex.scan(~r{do\(\).*?don't\(\)}, input)
+      |> Enum.map(fn l -> Enum.reduce(l, "", fn a, b -> a <> b end) end)
+      |> Enum.reduce("", fn a, b -> a <> b end)
+
+    calc_sum(s)
+    |> IO.inspect()
   end
 end
 
@@ -23,8 +35,8 @@ Day3.day3_1()
 runtime = NaiveDateTime.diff(NaiveDateTime.utc_now(), start, :microsecond)
 IO.puts("\nTook [#{runtime}] microseconds\n")
 
-# IO.puts("\npart2")
-# start = NaiveDateTime.utc_now()
-# Day3.day3_2()
-# runtime = NaiveDateTime.diff(NaiveDateTime.utc_now(), start, :microsecond)
-# IO.puts("\nTook [#{runtime}] microseconds\n")
+IO.puts("\npart2")
+start = NaiveDateTime.utc_now()
+Day3.day3_2()
+runtime = NaiveDateTime.diff(NaiveDateTime.utc_now(), start, :microsecond)
+IO.puts("\nTook [#{runtime}] microseconds\n")
